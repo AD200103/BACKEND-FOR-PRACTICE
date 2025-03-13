1a6adee765f70f2d792ddc9add287b7fe6c8f04b
Andrius200103 <cheekibreeki456845@gmail.com  git config --global user.name AD200103>
2025-03-13 21:05:58 +0200

    created mongo connection

---
+++ index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION as string)
+  .then(() => console.log("Connected!"))
+  .catch(() => console.log("Bad connection!"));
+
+app.use((req, res) => {
+  res.status(404).json({ message: "No such endpoint exists!" });
+});
+// const endpointNotFound: express.RequestHandler= (req,res) =>{
+//     res.status(404).json({ message: "No such endpoint exists!" });
+// }
+// app.use(endpointNotFound)
+
+app.listen(process.env.PORT, () => {
+  console.log(`Application started successfuly on port:${process.env.PORT}!`);
+});
